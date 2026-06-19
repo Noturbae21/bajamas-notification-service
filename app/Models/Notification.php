7dcc5c5296a6652218f8508ae20a2620be70b60a
Izzul <100136488+Noturbae21@users.noreply.github.com>
2026-06-19 16:57:43 +0700

    Buat proses notif sama model notif

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    // Daftarkan kolom yang boleh diisi secara massal
+    protected $fillable = [
+        'user_id',
+        'email',
+        'type',
+        'message',
+        'status',
+    ];
 }
